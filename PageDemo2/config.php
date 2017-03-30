79babbcd00993ca5e9f5b5d288c3dfcfd2f38200
malei <344739968@qq.com>
2017-03-30 18:09:40 +0800

    ++++conn读取全局变量配置

--- PageDemo2/config.php
+++ PageDemo2/config.php
@@ -8,10 +8,13 @@
 /*
 *配置数据库信息
 */
-$dbms='mysql';     //数据库类型
-$host='localhost'; //数据库主机名
-$dbName='test';    //使用的数据库
-$user='root';      //数据库连接用户名
-$pass='123456';    //对应的密码
+$dbms='mysql';       //数据库类型
+$host='localhost';  //数据库主机名
+$dbName='test';      //使用的数据库
+$user='root';        //数据库连接用户名
+$pass='123456';      //对应的密码
 $dsn="$dbms:host=$host;dbname=$dbName";
+define("dsn",$dsn);
+define("user",$user);
+define("pass",$pass);
 ?>
